124a00047c231f18489eb3bf6ca2a4ad7f46f81a
jeanamorim <jeanamorim08@hotmail.com>
2025-01-13 13:21:59 -0300

    feat: add runDas method for handling CNPJ requests and improve error handling

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import undetected_chromedriver as udc
 from fake_useragent import UserAgent
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from extension import proxies
+from selenium.webdriver.common.action_chains import ActionChains 
 
 logging.basicConfig(
     format="%(levelname)s:%(message)s",
@@ -30,27 +31,25 @@ app = Flask(__name__)
 class Prenota:
     @staticmethod
     def run(cnpj):
-        proxies_extension = proxies('spphm86cju', 'ik18bprcRnN4OxU=3d', 'gate.smartproxy.com', '7000')
-        chrome_options = udc.ChromeOptions()
-        chrome_options.headless = True
-        chrome_options.add_extension(proxies_extension)
-       
-        chrome_options.add_argument("--disable-gpu")  # Desabilita aceleração de GPU (necessário no modo headless)
-        chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Evita que o Chrome seja detectado como automatizado 
+        options = udc.ChromeOptions()
+        options.headless = True
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-blink-features=AutomationControlled")
         ua = UserAgent()
         user_agent = ua.random
-        chrome_options.add_argument(f'--user-agent={user_agent}')
-        driver = udc.Chrome(use_subprocess=True, options=chrome_options)
+        options.add_argument(f'--user-agent={user_agent}')
+        driver = udc.Chrome(use_subprocess=True, options=options)
         driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
             "source": """
             Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
             """
         })
-    
-       
-        try:            
+        try:
+            # URL com o CNPJ passado dinamicamente
             url = f'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/dasnsimei.app/mobile/{cnpj}'
             driver.get(url)
+            # Verifique a URL atual
+            time.sleep(3) 
             expected_url = 'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/dasnsimei.app/'
             WebDriverWait(driver, 20).until(EC.url_to_be(expected_url))
             cookies = driver.get_cookies()
@@ -62,17 +61,75 @@ class Prenota:
             driver.quit()
             return {"status": "error", "message": str(e)}
 
+    @staticmethod
+    def runDas(cnpj):
+        options = udc.ChromeOptions()
+        options.headless = True
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        ua = UserAgent()
+        user_agent = ua.random
+        options.add_argument(f'--user-agent={user_agent}')
+        driver = udc.Chrome(use_subprocess=True, options=options)
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": """
+            Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
+            """
+        })
+        try:
+            # URL com o CNPJ passado dinamicamente
+            url = f'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Identificacao'
+            driver.get(url)
+            time.sleep(2) 
+            # Verifique a URL atual
+            input_element = driver.find_element(By.CSS_SELECTOR, "#cnpj")
+            input_element.clear()
+            input_element.send_keys(cnpj)
+            time.sleep(2) 
+
+            continuar_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "#continuar")))
+            continuar_button.click()
+
+            time.sleep(2) 
+            current_url = driver.current_url
+            expected_url = 'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Home/Inicio'
+            WebDriverWait(driver, 20).until(EC.url_to_be(expected_url))
+            cookies = driver.get_cookies()
+            driver.quit()
+            return {"status": "success", "cookies": cookies}
+        except Exception as e:
+            logging.error(f"Exception: {e}")
+            driver.quit()
+            return {"status": "error", "message": str(e), "cookies": []}
 
 @app.route("/", methods=["POST"])
 def start_prenota():
+    # Obtém o CNPJ do corpo da requisição
     data = request.get_json()
     cnpj = data.get("cnpj")
+    print(cnpj)
+    
     if not cnpj:
         return jsonify({"status": "error", "message": "CNPJ is required"}), 400
 
+  
     response = Prenota.run(cnpj)
     return jsonify(response)
 
+@app.route("/das", methods=["POST"])
+def start_prenota_das():
+    # Obtém o CNPJ do corpo da requisição
+    data = request.get_json()
+    cnpj = data.get("cnpj")
+    print(cnpj)
+    
+    if not cnpj:
+        return jsonify({"status": "error", "message": "CNPJ is required"}), 400
+
+  
+    response = Prenota.runDas(cnpj)
+    return jsonify(response)
+
 if __name__ == "__main__":
-    # Configuração de produção
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
