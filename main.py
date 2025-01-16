3e43ea716f608c46e2b71f792ea391f175263c84
jeanamorim <jeanamorim08@hotmail.com>
2025-01-16 13:02:47 -0300

    feat: enhance Prenota class with DAS processing and cookie management

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 from selenium.common.exceptions import NoSuchElementException
 import logging
 import sys
@@ -22,7 +23,134 @@ import ultils
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 import base64
-from das import gerar_das, post_emissao, imprimir_das
+
+class MyHTTPAdapter(requests.adapters.HTTPAdapter):
+    def send(self, request, **kwargs):
+        print(f"\n[Request] {request.method} {request.url}")
+        print("Headers:", request.headers)
+        print("Body:", request.body)
+        response = super().send(request, **kwargs)
+        print(f"[Response] {response.status_code} {response.url}")
+        print("Headers:", response.headers)
+        print("Body:", response.text)
+        return response
+
+
+def get_data_atual():
+    current_date = datetime.now()
+    return current_date.strftime('%d/%m/%Y')
+
+def gerar_das(cookies, token, ano, mes):
+    url = 'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/emissao/gerarDas'   
+    data = {
+         "__RequestVerificationToken": token,
+        'ano': str(ano),
+        'pa': f"{ano}{mes}",
+        'dataConsolidacao': get_data_atual(),
+    }
+
+    try:
+        cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
+        session = requests.Session()
+        session.cookies.update(cookie_dict)
+        response = session.post(url, data=data)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            scripts = soup.find_all('script', string=lambda text: text and 'Notificacao.Show' in text)
+            mensagens = []
+
+            for script in scripts:
+                start_index = script.string.find("','") + 2  
+                end_index = script.string.find("');", start_index) 
+                if start_index != -1 and end_index != -1:
+                    mensagem = script.string[start_index:end_index].strip("'")
+                    mensagens.append(mensagem)
+
+            if len(mensagens) > 1:
+                return {"mensagem": mensagens[1] }
+            elif mensagens: 
+                return {"mensagem": mensagens[0] }
+            
+            return response.text
+        else:
+            return None
+    except Exception as error:
+        return None
+
+
+def post_emissao(cookies, ano):
+    url = 'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/emissao'
+    data = {
+        'ano': str(ano)
+    }
+    try:
+        cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
+        session = requests.Session()
+        session.cookies.update(cookie_dict)
+        response = session.post(url, data=data)
+        if response.status_code == 200:
+           soup = BeautifulSoup(response.text, 'html.parser')
+           csrf_token = soup.find('input', {'name': '__RequestVerificationToken'})
+           script = soup.find('script', string=lambda text: text and 'Notificacao.Show' in text)
+           if script:
+               start_index = script.string.find("','") + 2  # Localiza o início da mensagem
+               end_index = script.string.find("');", start_index)  # Localiza o final da mensagem
+               mensagem = script.string[start_index:end_index]
+               mensagem = mensagem.strip("'")
+               return { "mensagem": mensagem }
+           
+           if csrf_token:
+               token_value = csrf_token.get('value')
+               return token_value
+           else:
+               print("CSRF Token não encontrado.")
+           return None
+    except Exception as error:
+        print(f"Erro ao realizar o POST: {error}")
+        return None
+    
+def apurar_das(ano: int, cookies: str) -> dict:
+    url = "https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Emissao/VerificaRetificacaoAutomatica" 
+    form_data = {
+        'ano': str(ano),
+        'aliquotaDivergente': 'false',
+        'valorTributoDivergente': 'false'
+    }
+
+    lista_pa = [f"{ano}{str(mes).zfill(2)}" for mes in range(1, 13)]
+    form_data['listaPA'] = lista_pa
+
+    try:
+        cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
+        session = requests.Session()
+        session.cookies.update(cookie_dict)
+        response = session.post(url, data=form_data)
+
+        if response.status_code == 200:
+            return response.text
+        else:
+            return {"status": "error", "message": "Erro na requisição", "details": response.text}
+    except requests.exceptions.RequestException as error:
+        return {"status": "error", "message": str(error)}
+
+def imprimir_das(cookies: str) -> str:
+    url = "https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/emissao/imprimir" 
+    cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
+    session = requests.Session()
+    session.cookies.update(cookie_dict)
+    try:
+        response = session.get(url)
+        response.raise_for_status()  # Levanta um erro para status HTTP 4xx/5xx
+        
+        if response.status_code == 200:
+            recibo_pdf = base64.b64encode(response.content).decode('utf-8')
+            return recibo_pdf
+        else:
+            print(f"Erro na requisição GET: {response.status_code}")
+            return None
+    except requests.RequestException as error:
+        print(f"Erro ao realizar o GET: {error}")
+        return None
 
 
 app = Flask(__name__)
@@ -45,6 +173,7 @@ class Prenota:
             """
         })
         
+       
         try:
             url = 'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/dasnsimei.app/Identificacao'
             driver.get(url)
@@ -66,6 +195,13 @@ class Prenota:
             WebDriverWait(driver, 20).until(EC.url_to_be(expected_url))
             cookies = driver.get_cookies()
             cokiesString = cookies_to_string(cookies)
+            print(cokiesString)
+            
+            
+             
+            
+
+            print("Cookies:", cokiesString)
             driver.quit()
             return {"status": "success", "cookies": cookies}
         except Exception as e:
@@ -74,7 +210,7 @@ class Prenota:
             return {"status": "error", "message": str(e)}
 
     @staticmethod
-    def runDas(cnpj):
+    def runDas(cnpj, ano, isApuration):
         options = udc.ChromeOptions()
         options.headless = True
         options.add_argument("--disable-gpu")
@@ -105,10 +241,49 @@ class Prenota:
             expected_url = 'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Home/Inicio'
             WebDriverWait(driver, 20).until(EC.url_to_be(expected_url))
             cookies = driver.get_cookies()
-            token =  post_emissao(cookies)
-            response_data =  gerar_das(cookies, token, '2025', '01')
-            imprimir = imprimir_das(cookies)
-            return {"status": "success", "data": imprimir}
+            if not cookies:
+                return {"status": "error", "message": "No cookies found"}
+            
+            driver.quit()
+            
+            token = post_emissao(cookies, ano)
+
+            if "mensagem" in token and token["mensagem"]:
+                return {"status": "error", "message": token["mensagem"]}
+
+            if not token:
+                return {"status": "error", "message": "Erro ao obter o token CSRF"}
+            
+            if isApuration:
+                apurar = apurar_das(ano, cookies)
+                if apurar.strip('"') != "NO":
+                    return {"status": "error", "message": apurar}
+                
+            resultados = [] 
+
+            for mes in range(1, 13): 
+                mes_str = f"{mes:02d}" 
+
+                gerar = gerar_das(cookies, token, ano, mes_str)
+
+                if "mensagem" in gerar and gerar["mensagem"]:
+                    resultados.append({
+                    "mes": f"{ano}{mes:02d}",
+                    "message": gerar["mensagem"],
+                    })
+
+                else:
+
+                    imprimir = imprimir_das(cookies)
+
+                    resultados.append({
+                        "mes": f"{ano}{mes:02d}",
+                        "response_data": imprimir,
+                    })
+
+                logging.info(f"Processado mês {mes_str}")
+
+            return {"status": "success", "data": resultados}
           
         except Exception as e:
             logging.error(f"Exception: {e}")
@@ -134,13 +309,23 @@ def start_prenota_das():
     # Obtém o CNPJ do corpo da requisição
     data = request.get_json()
     cnpj = data.get("cnpj")
+    ano = data.get("ano")
+    isApuration = data.get("isApuration")
     print(cnpj)
     
+    # Validação do CNPJ
     if not cnpj:
         return jsonify({"status": "error", "message": "CNPJ is required"}), 400
 
-  
-    response = Prenota.runDas(cnpj)
+    # Validação do Ano
+    if not ano:
+        return jsonify({"status": "error", "message": "Ano is required"}), 400
+    
+    if isApuration is None or not isinstance(isApuration, bool):
+        return jsonify({"status": "error", "message": "isApuration must be a boolean"}), 400
+
+      
+    response = Prenota.runDas(cnpj, ano, isApuration)
     return jsonify(response)
 
 if __name__ == "__main__":
